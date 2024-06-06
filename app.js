d6bc798aec759bb3a9f1ffcc257db7f6c6649b90
huss0249 <ahmedsabry@outlook.com>
2024-06-05 22:14:07 -0400

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+import '/comps/comp-header.js'
+import '/comps/comp-main.js'
+import '/comps/comp-footer.js'
+
+window.wcUID = 0;
+
+const $app = document.querySelector('#app')
+
+// $app.innerHTML = `
+// <big-head></big-head>
+// <big-main></big-main>
+// <big-foot class='bg-info' >copyright text</big-foot>`
+// $app.innerHTML = '<big-bang></big-bang>'
+
+let df = new DocumentFragment()
+let $head = document.createElement('big-head')
+let $main = document.createElement('big-main')
+let $foot = document.createElement('big-foot')
+
+$foot.classList.add('bg-info')
+$foot.textContent = 'Copyright Text'
+
+df.appendChild($head)
+df.appendChild($main)
+df.appendChild($foot)
+
+$app.appendChild(df)
